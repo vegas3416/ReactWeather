ce04b6e18c1e0df2ecbeb5dd93e8d157702162cc
vegas3416 <vegas3416@gmail.com>
2017-05-23 14:52:42 -0500

    Adding pieces to center data

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page.</p>
+      <h1 className="text-centered">Examples</h1>
+      <p>Here are a few example locations to try out</p>
+      <ol>
+      	<li>
+      		<Link to='/?location=Austin'>Austin, TX</Link>
+      	</li>
+      	<li>
+      		<Link to='/?location=Mexico City, Mexico'>Mexico City</Link>
+      	</li>
+      </ol>
     </div>
 
   )
